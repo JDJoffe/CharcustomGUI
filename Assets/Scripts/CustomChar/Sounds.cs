181ebd136577489af9acab257170372d0da9acae
josh <48034855+JDJoffe@users.noreply.github.com>
2019-06-09 03:27:54 +1000

    button sounds

---
+++ Assets/Scripts/CustomChar/Sounds.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class Sounds : MonoBehaviour
+{
+    public AudioClip button;
+    public AudioSource buttonSource;
+
+    public void Button()
+    {
+        buttonSource.clip = button;
+        buttonSource.Play();
+    }
+}
